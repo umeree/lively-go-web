fd3486bdd753866587fde68a6b86ef549054d769
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2023-01-29 04:12:35 +0500

    Added api headers for cros-origins

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
 
-
 // Setting up config file
 if (process.env.NODE_ENV !== "PRODUCTION")
   require("dotenv").config({ path: "./config/config.env" });
 
-
 app.use(express.json());
+
+app.use((_, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 // Import all routes
 
@@ -15,4 +23,4 @@ const auth = require("./routes/auth");
 
 app.use("/api/v1", auth);
 
-module.exports = app;
+module.exports = app;
