17b95b978a53706753c7e88996fe25684c4dd663
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2023-01-29 03:21:54 +0500

    Completed register user ✅ & working on login user

--- routes/auth.js
+++ routes/auth.js
@@ -3,26 +3,56 @@ const express = require("express");
 const router = express.Router();
 const prisma = require("../lib/prisma");
 
-router.route("/login").get((req, res) => {
-  console.log("This is login route:", req);
-  res.status(200).json({
-    message: "This is login page, hello world" + req,
-  });
+router.route("/login_user").get((req, res) => {
+  console.log("login_user api hit!");
+
+  const { email, password } = req.body;
+
+  try {
+    const result = prisma.user
+      .findUnique({
+        where: {
+          email,
+        },
+      })
+      .then(() => {
+        console.log(result);
+        res.status(200).json({ message: "found user!" });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } catch (error) {
+    console.log("error while loging in user: ", error.message);
+    res.status(500).json({
+      error_message: "Error while loggin user!",
+    });
+  }
 });
 
 router.route("/register_user").post((req, res) => {
+  console.log("Register_user api hit!");
+
   const { first_name, last_name, email, password } = req.body;
+  console.log(req.body);
 
   try {
-    const result = prisma.user.create({
-      data: {
-        first_name,
-        last_name,
-        email,
-        password,
-      },
-    });
-    res.status(200).json({ message: result });
+    const result = prisma.user
+      .create({
+        data: {
+          first_name,
+          last_name,
+          email,
+          password,
+        },
+      })
+      .then(() => {
+        console.log(result);
+        res.status(200).json({ message: "created!" });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   } catch (error) {
     console.log("error while registering user: ", error.message);
     res.status(500).json({
