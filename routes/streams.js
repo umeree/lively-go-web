e09b9ffcaf82d5bb1e975a7a92cf465c1a6797e3
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2023-05-15 00:37:21 +0500

    Added send hearts api

--- routes/streams.js
+++ routes/streams.js
@@ -87,17 +87,14 @@ router.route("/update_stream").post((req, res) => {
     });
   }
 });
-// Get All streams
-
 
 router.route("/all_streams").get((req, res) => {
   try {
-    const result = prisma.Stream
-      .findMany({
-        where:{
-          status: "live"
-        }
-      })
+    const result = prisma.Stream.findMany({
+      where: {
+        status: "live",
+      },
+    })
       .then((query_res) => {
         if (query_res) {
           res.status(200).json({
@@ -114,6 +111,106 @@ router.route("/all_streams").get((req, res) => {
     console.log("error while getting users: ", error.message);
     res.status(500).json({
       error_message: "Error while getting users!",
+    });
+  }
+});
+
+router.route("/send_hearts").post(async (req, res) => {
+  if (req.method != "POST") {
+    return res.status(500).json({
+      error_message: "Method not allowed!",
+    });
+  }
+  const { userId, ownerId, hearts } = req.query;
+  if (!userId || !ownerId || !hearts) {
+    res.status(403).json({
+      message: "Userid, ownerId, and hearts number is manadatory!",
+    });
+  }
+  try {
+    const user = await prisma.user
+      .findUnique({
+        where: {
+          id: parseInt(userId),
+        },
+      })
+      .then((r) => {
+        if (r) {
+          return r;
+        } else {
+          res.status(404).json({
+            message: "user not found!",
+          });
+        }
+      });
+    const owner = await prisma.user
+      .findUnique({
+        where: {
+          id: parseInt(ownerId),
+        },
+      })
+      .then((r) => {
+        if (r) {
+          return r;
+        } else {
+          res.status(404).json({
+            message: "owner not found!",
+          });
+        }
+      });
+    const setHeartsToOwner = owner.hearts + parseInt(hearts);
+    const updateOwner = await prisma.user
+      .update({
+        where: {
+          id: parseInt(ownerId),
+        },
+        data: {
+          hearts: parseInt(setHeartsToOwner),
+        },
+      })
+      .then((r) => {
+        if (r) {
+          return r;
+        } else {
+          res.status(404).json({
+            message: "owner hearts failed",
+          });
+        }
+      });
+    const setHeartsToUser = user.hearts - hearts;
+    const updateUser = await prisma.user
+      .update({
+        where: {
+          id: parseInt(userId),
+        },
+        data: {
+          hearts: parseInt(setHeartsToUser),
+        },
+      })
+      .then((r) => {
+        if (r) {
+          return r;
+        } else {
+          res.status(404).json({
+            message: "user hearts failed",
+          });
+        }
+      });
+    if (user && owner && updateOwner && updateUser) {
+      res.status(200).json({
+        message: "Updated user and owner of stream with heart: " + hearts,
+      });
+    } else {
+      res.status(200).json({
+        message:
+          "Failed while updating user and owner of stream with heart: " +
+          hearts,
+      });
+    }
+  } catch (error) {
+    console.log("error sending hearts: ", error.message);
+    res.status(500).json({
+      error_message: "Error while sending hearts!",
     });
   }
 });
